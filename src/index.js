f8536c3e481d4fa9d0340d497d112c63f39c647d
Tee Parham <tee@neighborland.com>
2017-12-26 12:34:12 -0700

    React app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
 import Player from './player';
 import Game from './game';
 
 const play = () => Game().play(Player())
 
-export { Player, Game, play }
+class Pick4 extends Component {
+  render() {
+    return (
+      <p className="f5 tc lh-copy">
+        Pick 4
+      </p>
+    );
+  }
+}
+
+export { Player, Game, play, Pick4 }
+
+render(<Pick4 />, document.getElementById('app'))
